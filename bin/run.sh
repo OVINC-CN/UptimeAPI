b1b65d672af890b1c4805cdc68fd1f196502fe94
oren.zhang <oren.zhang@temu.com>
2024-10-17 11:38:40 +0800

    feat(process): change start script

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,7 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-nohup python manage.py celery worker -c 4 -l INFO >/dev/stdout 2>&1 &
-nohup python manage.py celery beat -l INFO >/dev/stdout 2>&1 &
+python manage.py celery worker -c 4 -l INFO
+python manage.py celery beat -l INFO
+python manage.py run_scheduler
 gunicorn --bind "[::]:8020" -w $WEB_PROCESSES --threads $WEB_THREADS -k uvicorn_worker.UvicornWorker --proxy-protocol --proxy-allow-from "*" --forwarded-allow-ips "*" entry.asgi:application
